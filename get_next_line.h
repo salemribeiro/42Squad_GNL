f21d6e22b7bc56a989e443f0c513e54d5e738f80
Salem Ribeiro <salem.ribeiro@sapo.pt>
2020-04-13 13:02:48 -0300

    leitura de uma linha

---
+++ get_next_line.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+char	*ft_strjoin(char*, char*);
+int		get_next_line(int, char**);
+char	*ft_calloc(size_t, size_t);
+int		check_line(char*);
