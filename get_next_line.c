8b0cf5a4c1d652053166bbe4afd165f00d447b22
Salem Ribeiro <salem.ribeiro@sapo.pt>
2020-04-16 00:05:20 -0300

    Código funcional pronto para os testes

--- get_next_line.c
+++ get_next_line.c
@@ -1,25 +1,40 @@
 #include "get_next_line.h"
 
-int get_next_line(int fd, char **line)
+/* Função principal para leitura de linha                                     */
+
+int		get_next_line(int fd, char **line)
 {
-	static char	*l_buffer;
+	static char	*s_line;
+	char		*l_buffer;
 	int			result;
 
+	free(*line);
+	*line = NULL;
+	l_buffer = (char*)ft_calloc((BUFFER_SIZE + 1), sizeof(char));
 	result = 0;
-	if (l_buffer[0])
-		l_buffer = (char*)calloc((BUFFER_SIZE +1), sizeof(char));
-	if (BUFFER_SIZE < 1 && fd < 3 && !l_buffer)
+	if (!s_line)
+		s_line = (char*)ft_calloc((BUFFER_SIZE + 1), sizeof(char));
+	if (BUFFER_SIZE < 1 && fd < 3 && !s_line)
 		return(-1);
-	while (!check_line(*line))
+	while (!check_line(s_line))
 	{
 		result = read(fd, l_buffer, BUFFER_SIZE);
-		if (result > 0)
-			*line = ft_strjoin(*line, l_buffer);
-		else if (result == -1)
+		if (result > 0 &&  result <= BUFFER_SIZE)
+			s_line = ft_strjoin(s_line, l_buffer);
+		else
+		{
+			*line = s_line;
 			return (-1);
+		}
 	}
+	s_line = cleanline(line, s_line);
+	free(l_buffer);
 	return (result);
 }
+
+
+
+/* Função responsável por unir as duas strings                                */
 
 char	*ft_strjoin(char *s1, char *s2)
 {
@@ -33,7 +48,7 @@ char	*ft_strjoin(char *s1, char *s2)
 		i++;
 	while (s2[j])
 		j++;
-	ptr = (char*)calloc(i + j + 1, sizeof(char));
+	ptr = (char*)ft_calloc(i + j + 1, sizeof(char));
 	i = 0;
 	while (s1[i])
 	{
@@ -48,6 +63,8 @@ char	*ft_strjoin(char *s1, char *s2)
 	}
 	return (ptr);
 }
+
+/* Função verifica se já temos uma linha completa                             */
 
 int		check_line(char *ptr)
 {
@@ -65,6 +82,10 @@ int		check_line(char *ptr)
 	return (newline);
 }
 
+/* Função responsável por alocar memória                                      */
+/* foi modificada para retornar um ponteiro de char e bzero foi incluido no   */
+/* código, evitando a implementação de mais uma função no arquivo             */
+
 char	*ft_calloc(size_t count, size_t size)
 {
 	char	*ptr;
@@ -81,4 +102,35 @@ char	*ft_calloc(size_t count, size_t size)
 		}
 	}
 	return (ptr);
+}
+
+size_t		ft_strlcpy(char *dest, char *src, size_t size)
+{
+	size_t i;
+	size_t j;
+
+	i = 0;
+	j = 0;
+	while (src[j] != '\0')
+		j++;
+	if (size > 0)
+	{
+		while (src[i] != '\0' && i < (size - 1))
+		{
+			dest[i] = src[i];
+			i++;
+		}
+		dest[i] = '\0';
+	}
+	return (j);
+}
+
+size_t		ft_strlen(const char *source)
+{
+	int i;
+
+	i = 0;
+	while (source[i] != '\0')
+		i++;
+	return (i);
 }
--- main.c
+++ main.c
@@ -5,11 +5,16 @@ int main()
 	static int	fd;
 	static char	*line;
 
-	line = (char*)calloc((BUFFER_SIZE +1), sizeof(char));
 	fd = open("./teste", O_RDONLY);
 	get_next_line(fd, &line);
-	printf("%s", line);
+	printf("%s\n", line);
 	get_next_line(fd, &line);
-	printf("%s", line);
+	printf("%s\n", line);
+	get_next_line(fd, &line);
+	printf("%s\n", line);
+	get_next_line(fd, &line);
+	printf("%s\n", line);
+	get_next_line(fd, &line);
+	printf("%s\n", line);
 	return (0);
 }
