068845e80dd3dedcf90f497e61a98a9f50888b1b
Salem Ribeiro <salem.ribeiro@sapo.pt>
2020-04-19 00:44:28 -0300

    Limpeza no arquivo main e remoção do arquivo teste0

--- main.c
+++ main.c
@@ -6,11 +6,10 @@ int main()
 	int			ret;
 	static char	*line;
 
-	fd = open("./teste0", O_RDONLY);
+	fd = open("./teste", O_RDONLY);
 	while ((ret = get_next_line(fd, &line)))
 	{
 		printf("%s\n", line);
 	}
-	printf("retorno: %d\n", ret);
 	return (0);
 }
